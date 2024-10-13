0b07b660074dff25ae849c4efdc8794a59de6f45
Urazov Aibek <ibek-94@mail.ru>
2024-10-13 21:01:45 +0500

    add

---
+++ test_QA.py
@@ -0,0 +1,27 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+#Поиск футера и кнопки-виджета на странице "Компания" https://only.digital/company
+browser = webdriver.Firefox()
+browser.implicitly_wait(2)
+
+browser.get("https://only.digital/company")
+
+#Поиск элемента футер по тегнейму
+try:
+    browser.find_element(By.TAG_NAME, "footer") 
+    print("Элемент footer присуствует на странице")
+except:
+    print("Элемент footer не найден на странице")
+
+#Поиск кнопки виджета по классау
+try:
+    browser.find_element(By.CLASS_NAME, "kUMIZv") 
+    print("Кнопка виджет присуствует на странице")
+except:
+    print("Кнопка виджет не найдена на странице")
+
+
+browser.quit()
+
